da5b9d870ae00607f911bd80035fda8a53c272c9
Sam Briesemeister <sam@analyticspros.com>
2013-08-19 00:51:22 -0700

    Build out support for Transaction, Social, Timing, etc

--- UniversalAnalytics/Tracker.py
+++ UniversalAnalytics/Tracker.py
@@ -30,10 +30,17 @@ class HTTPPost(object):
     def update(self, **opts):
         self.attribs.update(opts)
 
+    @classmethod
+    def fixUTF8(cls, data):
+        for key in data:
+            if isinstance(data[ key ], basestring):
+                data[ key ] = data[ key ].encode('utf-8')
+
     # Apply stored properties to the given dataset & POST to the configured endpoint 
     def send(self, **data):
         data.update(self.base_attribs)
         data.update(self.attribs)
+        self.fixUTF8(data)
         request = Request(
                 self.endpoint, 
                 data = urlencode(data), 
@@ -49,59 +56,16 @@ class HTTPPost(object):
 class Tracker(object):
     trackers = {}
     state = {}
-    data_mapping = {
-            'page': 'dp',
-            'path': 'dp',
-            'title': 'dt',
-            'location': 'dl',
-            'hostname': 'dh',
-            'noninteraction': 'ni',
-            'noninteractive': 'ni',
-            'non-interaction': 'ni',
-            'nonInteration': 'ni',
-            'client-id': 'cid',
-            'clientId': 'cid',
-            'clientid': 'cid',
-            'sessionControl': 'sc',
-            'session-control': 'sc',
-            'sessioncontrol': 'sc',
-            'referrer': 'dr',
-            'referer': 'dr',
-            'campaign': 'cn',
-            'campaignName': 'cn',
-            'campaign-name': 'cn',
-            'source': 'cs',
-            'campaignSource': 'cs',
-            'medium': 'cm',
-            'campaignMedium': 'cm',
-            'keyword': 'ck',
-            'campaignKeyword': 'ck',
-            'content': 'cc',
-            'campaignContent': 'cc',
-            'campaignID': 'ci',
-            'campaignId': 'ci',
-            'screenResolution': 'sr',
-            'resolution': 'sr',
-            'screen-resolution': 'sr',
-            'viewport': 'vp',
-            'viewportSize': 'vp',
-            'viewport-size': 'vp',
-            'encoding': 'de',
-            'documentEncoding': 'de',
-            'document-encoding': 'de',
-            'screenColors': 'sd',
-            'screen-colors': 'sd',
-            'colors': 'sd',
-            'langauge': 'ul',
-            'userLanguage': 'ul',
-            'user-language': 'ul',
-            'appName': 'an',
-            'app': 'an',
-            'app-name': 'an',
-            'appVersion': 'av',
-            'version': 'av',
-            'app-version': 'av'
-    }
+    data_mapping = {  }
+    valid_hittypes = ('pageview', 'event', 'social', 'appview', 'transaction', 'item', 'exception', 'timing')
+
+
+    @classmethod
+    def params(cls, base, *names):
+        for i in names:
+            cls.data_mapping[ i ] = base
+
+
 
     def __init__(self, account, name = None, client_id = None):
         self.account = account
@@ -116,9 +80,13 @@ class Tracker(object):
         data = {}
         data.update(self.state)
 
+        if hittype not in self.valid_hittypes:
+            raise KeyError('Unsupported Universal Analytics Hit Type')
+
         if hittype == 'pageview' and isinstance(args[0], basestring):
             data['dp'] = args[0] # page path
-        if hittype == 'event' and len(args) > 2:
+
+        if hittype == 'event' and len(args) > 1:
             data['ec'] = args[0] # event category
             data['ea'] = args[1] # event action
             if len(args) > 2 and isinstance(args[2], basestring):
@@ -126,7 +94,21 @@ class Tracker(object):
             if len(args) > 3 and isinstance(args[3], int):
                 data['ev'] = args[3] # event value
 
-        for item in args:
+        if hittype == 'social' and len(args) > 1:
+            data['sn'] = args[0] # social network
+            data['sa'] = args[1] # social action
+            if len(args) > 2 and isinstance(args[2], basestring):
+                data['st'] = args[2] # social target
+
+        if hittype == 'timing' and len(args) > 1:
+            data['utc'] = args[0] # timing category
+            data['utv'] = args[1] # timing variable
+            data['utt'] = args[2] # timing time (value)
+            if len(args) > 3 and isinstance(args[3], basestring):
+                data['utl'] = args[3] # timing label
+
+
+        for item in args: # process dictionary arguments
             if isinstance(item, dict):
                 for key, val in item.items():
                     if key in self.data_mapping:
@@ -134,7 +116,7 @@ class Tracker(object):
                     else:
                         data[ key ] = val
 
-        for key, val in opts:
+        for key, val in opts: # process attributes given as named arguments
             if key in self.data_mapping:
                 data[ self.data_mapping[ key ] ] = val
 
@@ -150,6 +132,73 @@ class Tracker(object):
         else:
             pass
 
+# Declaring name mappings for Measurement Protocol parameters
+Tracker.params('cid', 'client-id', 'clientId', 'clientid')
+Tracker.params('dp', 'page', 'path')
+Tracker.params('dt', 'title', 'pagetitle', 'pageTitle' 'page-title')
+Tracker.params('dl', 'location')
+Tracker.params('dh', 'hostname')
+Tracker.params('ni', 'noninteractive', 'noninteraction', 'nonInteraction')
+Tracker.params('sc', 'sessioncontrol', 'session-control', 'sessionControl')
+Tracker.params('dr', 'referrer', 'referer')
+
+# Campaign attribution
+Tracker.params('cn', 'campaign', 'campaignName', 'campaign-name')
+Tracker.params('cs', 'source', 'campaignSource', 'campaign-source')
+Tracker.params('cm', 'medium', 'campaignMedium', 'campaign-medium')
+Tracker.params('ck', 'keyword', 'campaignKeyword', 'campaign-keyword')
+Tracker.params('cc', 'content', 'campaignContent', 'campaign-content')
+Tracker.params('ci', 'campaignId', 'campaignID', 'campaign-id')
+
+# Technical specs
+Tracker.params('sr', 'screenResolution', 'screen-resolution', 'resolution')
+Tracker.params('vp', 'viewport', 'viewportSize', 'viewport-size')
+Tracker.params('de', 'encoding', 'documentEncoding', 'document-encoding')
+Tracker.params('sd', 'colors', 'screenColors', 'screen-colors')
+Tracker.params('ul', 'language', 'user-language', 'userLanguage')
+
+# Mobile app
+Tracker.params('an', 'appName', 'app-name', 'app')
+Tracker.params('cd', 'contentDescription', 'screenName', 'screen-name', 'content-description')
+Tracker.params('av', 'appVersion', 'app-version', 'version')
+
+# Ecommerce
+Tracker.params('ta', 'affiliation', 'transactionAffiliation', 'transaction-affiliation')
+Tracker.params('ti', 'transaction', 'transactionId', 'transaction-id')
+Tracker.params('tr', 'revenue', 'transactionRevenue', 'transaction-revenue')
+Tracker.params('ts', 'shipping', 'transactionShipping', 'transaction-shipping')
+Tracker.params('tt', 'tax', 'transactionTax', 'transaction-tax')
+Tracker.params('cu', 'currency', 'transactionCurrency', 'transaction-currency') # Currency code, e.g. USD, EUR
+Tracker.params('in', 'item-name', 'itemName')
+Tracker.params('ip', 'item-price', 'itemPrice')
+Tracker.params('iq', 'item-quantity', 'itemQuantity')
+Tracker.params('ic', 'item-code', 'sku', 'itemCode')
+Tracker.params('iv', 'item-variation', 'item-category', 'itemCategory', 'itemVariation')
+
+# Social
+Tracker.params('sa', 'social-action', 'socialAction')
+Tracker.params('sn', 'social-network', 'socialNetwork')
+Tracker.params('st', 'social-target', 'socialTarget')
+
+# Exceptions
+Tracker.params('exd', 'exception-description', 'exceptionDescription')
+Tracker.params('exf', 'exception-fatal', 'exceptionFatal')
+
+# User Timing
+Tracker.params('utc', 'timingCategory', 'timing-category')
+Tracker.params('utv', 'timingVariable', 'timing-variable')
+Tracker.params('utt', 'time', 'timingTime', 'timing-time')
+Tracker.params('utl', 'timingLabel', 'timing-label')
+Tracker.params('dns', 'timingDNS', 'timing-dns')
+Tracker.params('pdt', 'timingPageLoad', 'timing-page-load')
+Tracker.params('rrt', 'timingRedirect', 'timing-redirect')
+Tracker.params('tcp', 'timingTCPConnect', 'timing-tcp-connect')
+Tracker.params('srt', 'timingServerResponse', 'timing-server-response')
+
+# Custom dimensions and metrics
+for i in range(0,200):
+    Tracker.params('cd{0}'.format(i), 'dimension{0}'.format(i))
+    Tracker.params('cm{0}'.format(i), 'metric{0}'.format(i))
 
 
 def create(account, name = None):
--- test.py
+++ test.py
@@ -12,6 +12,28 @@ tracker = Tracker.create('UA-8705807-11', name = 'mytracker')
 tracker.set('campaignName', 'testing')
 tracker.set('campaignMedium', 'testing')
 tracker.send('pageview', '/test')
-tracker.send('event', 'mycat', 'myact', 'mylbl', { 'noninteraction': 1 })
+tracker.send('event', 'mycat', 'myact', 'mylbl', { 'noninteraction': 1, 'page': '/1' })
+tracker.send('social', 'facebook', 'test', '/test#social')
+
+tracker.send('item', {
+    'transactionId': '12345abc',
+    'itemName': 'pizza',
+    'itemCode': 'abc',
+    'itemCategory': 'hawaiian',
+    'itemPrice': 24.55,
+    'itemQuantity': 1
+})
+
+tracker.send('transaction', {
+    'transactionId': '12345abc',
+    'transactionAffiliation': 'phone order',
+    'transactionRevenue': 28.00,
+    'transactionTax': 3.00,
+    'transactionShipping': 0.45,
+    'transactionCurrency': 'USD'
+})
+
+
+
 
 # vim: set nowrap tabstop=4 shiftwidth=4 softtabstop=0 expandtab textwidth=0 filetype=python foldmethod=indent foldcolumn=4
