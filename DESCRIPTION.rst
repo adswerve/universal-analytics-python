7bdf16512ae7582766bc2739e73f78e2a96aa4a1
Sam Briesemeister <sam@analyticspros.com>
2013-11-09 22:27:47 -0800

    Update <setup.py> for distribution through PyPi

---
+++ DESCRIPTION.rst
@@ -0,0 +1,11 @@
+Universal Analytics for Python
+==============================
+An interface for the Universal Analytics Measurement Protocol, modeled 
+(loosely) after Google's ``analytics.js``.
+
+This project is reasonably feature-complete for most use-cases,
+covering all relevant features of the Measurement Protocol, however we
+still consider it *beta*. Please feel free to file issues for feature
+requests.
+
+__
--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from setuptools import setup
 
+VERSION="0.1"
+
 try:
-    long_description=open('README.md', 'rt').read()
+    long_description=open('DESCRIPTION.rst', 'rt').read()
 except Exception:
-    long_description=""
+    long_description="Universal Analytics in Python; an implementation of Google's Universal Analytics Measurement Protocol"
+
+
 
 setup(
     name = "universal-analytics-python",
     description = "Universal Analytics Python Module",
     long_description = long_description,
 
-    version = "0.1",
+    version = VERSION,
+
     author = 'Sam Briesemeister',
+    author_email = 'opensource@analyticspros.com',
+
+    url = 'https://github.com/analytics-pros/universal-analytics-python',
+    download_url = "https://github.com/analytics-pros/universal-analytics-python/tarball/" + VERSION,
+
     license = 'BSD',
     packages = ["UniversalAnalytics"],
 
